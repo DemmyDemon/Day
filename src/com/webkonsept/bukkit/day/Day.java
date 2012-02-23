cfc55f1e0cf4f07cc589430b884b103a2f45865b
Fredrik Vold <fredrik@webkonsept.com>
2012-02-23 01:26:56 +0100

    Implemented configurability
    Including translatability, actually.

--- src/com/webkonsept/bukkit/day/Day.java
+++ src/com/webkonsept/bukkit/day/Day.java
@@ -8,65 +8,94 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.webkonsept.KonseptConfig;
+
+
 public class Day extends JavaPlugin {
+    private final String pluginName = "Day";
+    private String pluginVersion = "???";
 	private Logger log = Logger.getLogger("Minecraft");
 	private HashMap<String,Long> lastUsed = new HashMap<String,Long>();
+	private KonseptConfig cfg;
 	
 	@Override
 	public void onDisable() {
-		this.out("Disabled");
+		out("Disabled");
 	}
 
 	@Override
 	public void onEnable() {
-		this.out("Enabled");
+		pluginVersion = getDescription().getVersion();
+		cfg = new KonseptConfig(this);
+		cfg.refresh();
+	    out("Enabled");
 	}
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		if (!this.isEnabled()) return false;
 		
 		if (command.getName().equalsIgnoreCase("day")){
+		    int morning = cfg.get().getInt("morning",250);
 			if (sender instanceof Player){
 				Player player = (Player) sender;
 				
-				if (this.permit(player, "day.command.day")){
-					broadcast(player,"summoned the daystar");
-					player.getWorld().setTime(250);
+				if (player.hasPermission("day.command.day")){
+				    if (throttle(player)){
+				        broadcast(player,cfg.tr("playerSummon"));
+				        player.getWorld().setTime(morning);
+				    }
+				    else {
+				        player.sendMessage(ChatColor.RED+cfg.tr("playerThrottle"));
+				    }
 				}
 				else {
-					sender.sendMessage("Sorry!  Permission to force the sun up denied!  Switching too fast?");
+					sender.sendMessage(ChatColor.RED+cfg.tr("commandDenied"));
 				}
 			}
 			else {
 				for (World world : getServer().getWorlds()){
-					world.setTime(250);
-					sender.sendMessage(world.getName()+" bathed in sunlight");
+					world.setTime(morning);
 				}
-				getServer().broadcastMessage(ChatColor.DARK_PURPLE+"The Daystar was summoned.");
+				getServer().broadcastMessage(ChatColor.DARK_PURPLE+cfg.tr("consoleSummon"));
 			}
 			return true;
 		}
 		else if (command.getName().equalsIgnoreCase("night")){
+		    int evening = cfg.get().getInt("evening",14250);
 			if (sender instanceof Player){
 				Player player = (Player) sender;
-				if (this.permit(player, "day.command.night")){
-					broadcast(player,"banished the light");
-					player.getWorld().setTime(14250);
+				if (player.hasPermission("day.command.night")){
+				    if (throttle(player)){
+				        broadcast(player,cfg.tr("playerBanish"));
+				        player.getWorld().setTime(evening);
+				    }
+				    else {
+				        player.sendMessage(ChatColor.RED+cfg.tr("playerThrottle"));
+				    }
 				}
 				else {
-					player.sendMessage("Sorry!  Permission to force the sun down denied!  Switching too fast?");
+					player.sendMessage(cfg.tr("commandDenied"));
 				}
 			}
 			else {
 				for (World world : getServer().getWorlds()){
-					world.setTime(14250); 
-					sender.sendMessage(world.getName()+" plunged into darkness");
+					world.setTime(evening); 
 				}
-				this.getServer().broadcastMessage(ChatColor.DARK_PURPLE+"The world was plunged into darkness.");
+				this.getServer().broadcastMessage(ChatColor.DARK_PURPLE+cfg.tr("consoleBanish"));
 			}
 			return true;
+		}
+		else if (command.getName().equalsIgnoreCase("dayreload")){
+		    if (sender.hasPermission("day.command.reload")){
+		        cfg.refresh();
+		        sender.sendMessage(ChatColor.GOLD+cfg.tr("commandReloaded"));
+		    }
+		    else{
+		        sender.sendMessage(ChatColor.RED+cfg.tr("commandDenied"));
+		    }
+		    return true;
+		        
 		}
 		else {
 			return false;
@@ -77,34 +106,24 @@ public class Day extends JavaPlugin {
 			playerOnline.sendMessage(ChatColor.DARK_PURPLE+player.getName()+" "+whatHeDid);
 		}
 	}
-	public boolean permit(Player player,String permission){ 
-		boolean allow = false; // Default to GTFO
-		boolean timeOK = false;  // Assume it's throttled
-		
-		long limit = System.currentTimeMillis() - 10000; // Throttle at 10 seconds (10k milliseconds)
-		
-		if (!lastUsed.containsKey(player.getName()) || lastUsed.get(player.getName()) < limit ){
-			timeOK = true;
-			lastUsed.put(player.getName(),System.currentTimeMillis());
-		}
-		
-		if (player.hasPermission(permission)){
-			allow = true;
-		}
-		
-		if (timeOK){
-			return allow;
-		}
-		else {
-			return false;
-		}
+	private boolean throttle(Player player) {
+	    boolean timeOK = false;
+	    long limit = System.currentTimeMillis() - (cfg.get().getInt("throttleSeconds",10) * 1000); // *1000 because currentTime[[MILLIS]], yeah?
+	    if (!lastUsed.containsKey(player.getName()) || lastUsed.get(player.getName()) < limit ){
+	        timeOK = true;
+	        lastUsed.put(player.getName(),System.currentTimeMillis());
+        }
+        return timeOK;
 	}
 	public void out(String message) {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		log.info("[" + pdfFile.getName()+ " " + pdfFile.getVersion() + "] " + message);
+	    log.info("["+pluginName+" "+pluginVersion+"] "+message);
 	}
-	public void crap(String message){
-		PluginDescriptionFile pdfFile = this.getDescription();
-		log.severe("[" + pdfFile.getName()+ " " + pdfFile.getVersion() + " CRAP] " + message);
+	public void verbose(String message){
+	    if (cfg.get().getBoolean("verbose",false)){
+	        log.info("["+pluginName+" "+pluginVersion+" VERBOSE] "+message);
+	    }
+	}
+	public void error(String message){
+		log.severe("["+pluginName+" "+pluginVersion+" ERROR] "+message);
 	}
 }
